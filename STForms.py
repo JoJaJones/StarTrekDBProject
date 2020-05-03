98e8e13f434e3c3e7f867accfbb8ebd0f749033d
Josh Jones <jonesjo7@oregonstate.edu>
2020-05-02 22:30:18 -0700

    Add results table

--- STForms.py
+++ STForms.py
@@ -10,5 +10,15 @@ class SingleFieldForm(FlaskForm):
     field = StringField("")
     submit = SubmitField("Submit")
 
+class DeleteForm(FlaskForm):
+
+    submit = SubmitField("Delete")
+
+    def __init__(self, id, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.id = id
+
+
+
 class DoubleFieldForm(FlaskForm):
     pass
