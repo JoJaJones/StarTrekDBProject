5f682d9360815f1f0aeefa44f8d10c5176a0ed27
Josh Jones <jonesjo7@oregonstate.edu>
2020-05-02 19:50:39 -0700

    create all route added

--- StarTrekDB.py
+++ StarTrekDB.py
@@ -6,6 +6,27 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = SECRET
 
+@app.route("/create-all-tables")
+def init_DB():
+    password = request.args.get("pass")
+    result = "Invalid password"
+    if password == "picard":
+        results = "Tables created: "
+        db = connect_to_database()
+        for table in TABLES:
+            query = f"DROP TABLE IF ExISTS {table};"
+            results = execute_query(db, query)
+            query = TABLES[table]
+            results = execute_query(db, query)
+            if results[-2] != ":":
+                results += ", "
+
+            results += table
+
+    return results
+
+
+
 @app.route("/create-table")
 def create_table():
     table = request.args.get("table")
