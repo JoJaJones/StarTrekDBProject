0f12ae4810911d0abbb4512c3121cfb70b6b60b5
Josh Jones <jonesjo7@oregonstate.edu>
2020-05-02 23:22:20 -0700

    Fix typing bug

--- StarTrekDB.py
+++ StarTrekDB.py
@@ -35,7 +35,7 @@ def init_DB():
 @app.route("/add-species", methods=["GET", "POST"])
 def add_species():
     form = SingleFieldForm()
-    form.field.label = "Name: "
+    form.field.label = "Species Name: "
     query_res = False
     db = connect_to_database()
     columns = ["Species"]
@@ -60,6 +60,33 @@ def add_species():
 
     return render_template("single_field_add_form.html", form=form, query_res=query_res, column_names=columns)
 
+@app.route("/add-affiliations", methods=["GET", "POST"])
+def add_affiliations():
+    form = SingleFieldForm()
+    form.field.label = "Affiliation Name: "
+    query_res = False
+    db = connect_to_database()
+    columns = ["Affiliation"]
+
+    if form.validate_on_submit():
+        species = str(form.field.data)
+        form.field.data = ""
+
+        query = "INSERT INTO affiliations(name) VALUES (%s)"
+        res = execute_query(db, query, tuple([species]))
+
+    if "delete_no" in request.args:
+        query = f"DELETE FROM affiliations WHERE id = {request.args['delete_no']}"
+        res = execute_query(db, query)
+
+    query = "SELECT id, name FROM affiliations"
+    res = execute_query(db, query)
+
+    query_res = []
+    for item in res:
+        query_res.append(Row(item[0], item[1:]))
+
+    return render_template("single_field_add_form.html", form=form, query_res=query_res, column_names=columns)
 
 
 
