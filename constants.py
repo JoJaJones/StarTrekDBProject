b6be55c2d339481f64964a05ea3e45be1b281079
sotelom <sotelom>
2020-05-09 12:59:58 -0700

    Updated prepopulate data

--- constants.py
+++ constants.py
@@ -96,35 +96,96 @@ VIEW_COLUMNS = {
 }
 
 PREPOPULATE = {
-    '1': "INSERT INTO characters"
-         "(fname,lname,title,description,biography)"
-         "VALUES"
-         "('James','Kirk','Commanding Officer','Captain','James Tiberius Kirk was born in Riverside, Iowa, on March 22, 2233');",
+    '1': f"INSERT INTO {CHARACTERS}"
+          "(fname,lname,title,description,biography)"
+          "VALUES"
+          "('James','Kirk','Commanding Officer','Chief of Starfleet Operations','James Tiberius Kirk was born in Riverside, Iowa, on March 22, 2233');",
 
-    '2': "INSERT INTO characters"
-         "(fname,lname,title,description)"
-         "VALUES"
-         "('Hirakaru','Sulu','Captain','Commanding Officer');",
+    '2': f"INSERT INTO {CHARACTERS}"
+          "(fname,lname,title,description)"
+           "VALUES"
+         "('Hirakaru','Sulu','Captain','USS Enterprise helmsman');",
 
-    '3': "INSERT INTO characters"
-         "(fname,lname,title)"
-         "VALUES"
-         "('Nyota','Uhura','Commander');",
+    '3': f"INSERT INTO {CHARACTERS}"
+          "(fname,lname,title)"
+          "VALUES"
+          "('Nyota','Uhura','Lieutenant Commander');",
 
-    '4': "INSERT INTO characters"
-         "(fname,title)"
-         "VALUES"
-         "('Spock','Ambassador');",
+    '4': f"INSERT INTO {CHARACTERS}"
+          "(fname,title)"
+          "VALUES"
+          "('Spock','Ambassador');",
 
-    '5': "INSERT INTO actors"
-         "(fname,lname,birthday,imdb,cid)"
-         "VALUES"
-         "('William','Shatner','1931-03-22','https://www.imdb.com/name/nm0000638/',(SELECT id from characters WHERE lname='Kirk')),"
-         "('George','Takei','1937-04-20','https://www.imdb.com/name/nm0001786/',(SELECT id from characters WHERE lname='Sulu'));",
+    '5': f"INSERT INTO {ACTORS}"
+          "(fname,lname,birthday,imdb,cid)"
+          "VALUES"
+          "('William','Shatner','1931-03-22','https://www.imdb.com/name/nm0000638/',(SELECT id from characters WHERE lname='Kirk')),"
+          "('George','Takei','1937-04-20','https://www.imdb.com/name/nm0001786/',(SELECT id from characters WHERE lname='Sulu'));",
 
-    '6': "INSERT INTO actors"
-         "(fname,lname,birthday)"
-         "VALUES"
-         "('LeVar','Burton','1957-02-16'),"
-         "('Patrick','Stewart','1940-07-13');"
+    '6': f"INSERT INTO {ACTORS}"
+          "(fname,lname,birthday)"
+          "VALUES"
+          "('LeVar','Burton','1957-02-16'),"
+          "('Patrick','Stewart','1940-07-13');",
+
+    '7': f"INSERT INTO {AFFILIATIONS}"
+          "(name)"
+          "VALUES"
+          "('Starfleet'),"
+          "('Borg'),"
+          "('Ferengi'),"
+          "('Klingon'),"
+          "('Romulan');",
+
+    '8': f"INSERT INTO {SPECIES}"
+          "(name)"
+          "VALUES"
+          "('Borg'),"
+          "('Breen'),"
+          "('Human'),"
+          "('Vulcan'),"
+          "('Hirogen');",
+
+    '9': f"INSERT INTO {LOCATIONS}"
+          "(name,type)"
+          "VALUES"
+          "('Omicron Kappa II','planet'),"
+          "('Boreth','planet'),"
+          "('Earth','planet'),"
+          "('USS Enterprise','ship'),"
+          "('Borg Cube','ship'),"
+          "('USS Voyager','ship'),"
+          "('Deep Space 9','station');",
+
+    '10': f"INSERT INTO {SERIES}"
+           "(name,start_date,end_date)"
+           "VALUES"
+           "('The Original Series','1966-09-08','1968-09-20'),"
+           "('The Next Generation','1987-09-28','1993-09-20'),"
+           "('Voyager','1995-01-16','2000-10-04');",
+
+    '11': f"INSERT INTO {SERIES}"
+           "(name,start_date)"
+           "VALUES"
+           "('Discovery','2017-09-24');"
+
+    '12': f"INSERT INTO {CHAR_SPECIES}"
+           "(cid,sid)"
+           "VALUES"
+           "((SELECT id from characters WHERE lname='Kirk'),(SELECT id from species WHERE name='Human')),"
+           "((SELECT id from characters WHERE fname='Spock'),(SELECT id from species WHERE name='Vulcan')),"
+           "((SELECT id from characters WHERE fname='Spock'),(SELECT id from species WHERE name='Human')),"
+           "((SELECT id from characters WHERE lname='Sulu'),(SELECT id from species WHERE name='Human')),"
+           "((SELECT id from characters WHERE lname='Uhura'),(SELECT id from species WHERE name='Human'));",
+
+    '13': f"INSERT INTO {CHAR_SERIES}"
+           "(cid,sid)"
+           "VALUES"
+           "((SELECT id from characters WHERE lname='Kirk'),(SELECT id from series WHERE name='The Original Series')),"
+           "((SELECT id from characters WHERE lname='Kirk'),(SELECT id from series WHERE name='The Next Generation')),"
+           "((SELECT id from characters WHERE fname='Spock'),(SELECT id from series WHERE name='The Original Series')),"
+           "((SELECT id from characters WHERE fname='Spock'),(SELECT id from series WHERE name='The Next Generation')),"
+           "((SELECT id from characters WHERE lname='Sulu'),(SELECT id from series WHERE name='The Original Series')),"
+           "((SELECT id from characters WHERE lname='Uhura'),(SELECT id from series WHERE name='The Original Series'));"
+
 }
